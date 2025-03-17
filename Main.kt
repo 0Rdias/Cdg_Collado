790d1032ad4be4848660b0253972585cdd4612f2
0Rdias <raul.ds.ds64nes@gmail.com>
2025-03-17 17:40:21 -0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,28 @@
+fun main(args: Array<String>) {
+    fun main(args: Array<String>) {
+        println("Hello World!")
+
+        val senha = "123";
+        if (senha == "123"){
+            println ("acesso concedido");
+        }else{
+            println ("Senha incorreta");
+        }
+
+        val a = 5;
+        val b = 3;
+        if (a > b){
+            println("$a é maior que $b");
+        }
+        val x= 4;
+        when (x) {
+            1 ->println("1==x");
+            2 ->println("2==x");
+            else -> {
+                println("x tem outro valor")
+            }
+
+
+        }
+    }
+}
